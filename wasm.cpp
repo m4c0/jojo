3954edd35b33ba3098a59c0a9fdd8da29f3430e6
Eduardo Costa <m4c0@users.noreply.github.com>
2025-08-27 17:48:51 +0100

    wasm: fix support for both array and cstr

--- wasm.cpp
+++ wasm.cpp
@@ -3,38 +3,56 @@
 module jojo;
 import vaselin;
 
-using buf_t = hai::array<char>;
-using fn_t = hai::fn<void, void *, buf_t &>;
-
 extern "C" __attribute__((import_module("jojo"), import_name("read"))) void jojo_read(const char * name, unsigned nsz, void * ptr, unsigned idx);
 
-static hai::array<fn_t> g_in_flights{16};
-static hai::array<buf_t> g_buffers{16};
+struct slot {
+  hai::cstr cstr_buf;
+  hai::fn<void, void *, hai::cstr &> cstr_fn;
+
+  hai::array<char> array_buf;
+  hai::fn<void, void *, hai::array<char> &> array_fn;
+};
+static hai::array<slot> g_in_flights { 16 };
 
 EXP("jojo_alloc") void * alloc(unsigned idx, unsigned sz) {
-  auto & b = g_buffers[idx] = buf_t{sz};
-  return b.begin();
+  auto & s = g_in_flights[idx];
+  if (s.cstr_fn) {
+    s.cstr_buf = hai::cstr { sz };
+    return s.cstr_buf.begin();
+  } else {
+    s.array_buf = hai::array<char> { sz };
+    return s.array_buf.begin();
+  }
 }
 EXP("jojo_callback") void callback(void * ptr, unsigned idx) {
-  auto &fn = g_in_flights[idx];
-  auto &buf = g_buffers[idx];
-
-  fn(ptr, buf);
-  fn = {};
-  buf = {};
+  auto &s = g_in_flights[idx];
+  if (s.cstr_fn) s.cstr_fn(ptr, s.cstr_buf);
+  else s.array_fn(ptr, s.array_buf);
+  s = {};
 }
 EXP("jojo_err_callback") void err_callback(void * ptr, unsigned idx) {
   jojo::err_callback(ptr, "Request failed");
   g_in_flights[idx] = {};
-  g_buffers[idx] = {};
 }
 
-void jojo::read(jute::view name, void * ptr, fn_t fn) {
+void jojo::read(jute::view name, void * ptr, hai::fn<void, void *, hai::array<char> &> fn) {
   for (auto i = 0; i < g_in_flights.size(); i++) {
     auto & p = g_in_flights[i];
-    if (p) continue;
+    if (p.cstr_fn || p.array_fn) continue;
 
-    p = fn;
+    p.array_fn = fn;
+    jojo_read(name.begin(), name.size(), ptr, i);
+    return;
+  }
+
+  err_callback(ptr, "Too many open files");
+}
+void jojo::read(jute::view name, void * ptr, hai::fn<void, void *, hai::cstr &> fn) {
+  for (auto i = 0; i < g_in_flights.size(); i++) {
+    auto & p = g_in_flights[i];
+    if (p.cstr_fn || p.array_fn) continue;
+
+    p.cstr_fn = fn;
     jojo_read(name.begin(), name.size(), ptr, i);
     return;
   }
