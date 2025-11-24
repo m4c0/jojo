51dbea61cc52ce0661e9431da33cf8178639eaf4
Eduardo Costa <m4c0@users.noreply.github.com>
2025-11-24 14:05:32 +0000

    adding an example of using the "ptr"

--- poc.cpp
+++ poc.cpp
@@ -6,14 +6,16 @@ import jojo;
 import jute;
 import print;
 
+static int id = 0;
+
 int main() {
-  jojo::on_error([](void *, jute::view msg) {
-    erran("Error:", msg);
+  jojo::on_error([](void * ptr, jute::view msg) {
+    if (ptr == &id) erran("Error:", msg);
   });
 
   // In async platforms (wasm, etc) it calls sometime in the future
   // In sync platforms (non-wasm :) it calls immediately
-  jojo::read("poc.txt", nullptr, [](void * ptr, hai::cstr & buf) {
-    puta(buf);
+  jojo::read("poc.txt", &id, [](void * ptr, hai::cstr & buf) {
+    if (ptr == &id) puta(buf);
   });
 }
