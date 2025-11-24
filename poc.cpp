0d5c39be1a60ffa6e39cf179c041b0dbbbc4404c
Eduardo Costa <m4c0@users.noreply.github.com>
2025-11-24 15:17:34 +0000

    extending the example a bit

--- poc.cpp
+++ poc.cpp
@@ -6,16 +6,28 @@ import jojo;
 import jute;
 import print;
 
-static int id = 0;
+static int id_a = 0;
+
+struct b {
+  const int check = 'CAFE';
+} id_b;
 
 int main() {
+  // Showing multiple ways of identifying errors using a single error function.
   jojo::on_error([](void * ptr, jute::view msg) {
-    if (ptr == &id) erran("Error:", msg);
+    if (ptr == &id_a) erran("Error from consumer A:", msg);
+    if (static_cast<b *>(ptr)->check == 'CAFE') erran("Error from consumer B:", msg);
   });
 
   // In async platforms (wasm, etc) it calls sometime in the future
   // In sync platforms (non-wasm :) it calls immediately
-  jojo::read("poc.txt", &id, [](void * ptr, hai::cstr & buf) {
-    if (ptr == &id) puta(buf);
+  jojo::read("poc.txt", &id_a, [](void * ptr, hai::cstr & buf) {
+    putan("Data from A", buf);
   });
+  jojo::read("poc.txt", &id_b, [](void * ptr, hai::cstr & buf) {
+    putan("Data from B", buf);
+  });
+
+  jojo::read("poca.txt", &id_a, [](void * ptr, hai::cstr & buf) {});
+  jojo::read("poca.txt", &id_b, [](void * ptr, hai::cstr & buf) {});
 }
